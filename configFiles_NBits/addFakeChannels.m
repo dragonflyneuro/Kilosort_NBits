03cfcdf36ff6d23f0060b6477d5bce0591a56924
Daniel Ko <dsdanielko@gmail.com>
2023-11-07 09:52:27 +0000

    fixes to single channel sorting
    added script to add fake channels in either 0s or copies of channel
    in progress editing gpuFilter

---
+++ configFiles_NBits/addFakeChannels.m
@@ -0,0 +1,20 @@
+%% Change these
+
+fN = 'E:\ks_test\2020-10-28_10-42-20_INTERLACED.bin';
+nChOriginal = 1; % how many channels in original data?
+
+%%
+fid = fopen(fN, 'r');
+d = fread(fid,[nChOriginal,inf],"int16");
+% d(nChOriginal+1:4,:) = 0;
+d = repmat(d,4/nChOriginal,1);
+fclose(fid);
+
+fid = fopen([fN(1:end-4) '_kilosortAugmented.bin'], 'w');
+if fid < 0
+    warning('Data could not be saved')
+    return;
+end
+fwrite(fid, d, 'int16','l'); % little endian write
+fclose(fid);
+disp('Data saved!');
